f0a32c2a3f97d7ac18e2d051b4704d2bfcda2a67
asp143 <mungcs09@gmail.com>
2020-10-06 08:26:20 +0800

    Refactor network file

--- src/bin/www.ts
+++ src/bin/www.ts
@@ -1,73 +1,15 @@
 #!/usr/bin/env node
+/* eslint-disable no-console */
+/* eslint-disable func-style */
+/* eslint-disable no-use-before-define */
 
 /**
  * Module dependencies.
  */
 
-import app from '../app';
+// const http = require('http');
 import http from 'http';
-
-
-/**
- * Normalize a port into a number, string, or false.
- */
-
-function normalizePort(val: string) {
-  const port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
-
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-
-  return false;
-}
-
-/**
- * Event listener for HTTP server "error" event.
- */
-
-function onError(error: any) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-
-  const bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-}
-
-
-/**
- * Event listener for HTTP server "listening" event.
- */
-
-function onListening() {
-  const addr: any = server.address();
-  const bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  console.log('Listening on ' + bind);
-}
+import app from '../app';
 
 /**
  * Get port from environment and store in Express.
@@ -89,3 +31,65 @@ const server = http.createServer(app);
 server.listen(port);
 server.on('error', onError);
 server.on('listening', onListening);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val: string) {
+    // eslint-disable-next-line no-shadow
+    const port = parseInt(val, 10);
+
+    // eslint-disable-next-line no-restricted-globals
+    if (isNaN(port)) {
+    // named pipe
+        return val;
+    }
+
+    if (port >= 0) {
+    // port number
+        return port;
+    }
+
+    return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error: any) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    const bind = typeof port === 'string'
+        ? `Pipe ${port}`
+        : `Port ${port}`;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(`${bind} requires elevated privileges`);
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(`${bind} is already in use`);
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+    const addr: any = server.address();
+    const bind = typeof addr === 'string'
+        ? `pipe ${addr}`
+        : `port ${addr.port}`;
+    console.log(`Listening on${bind}`);
+}
