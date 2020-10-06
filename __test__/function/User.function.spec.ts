ddeca2d6b03ceca7b7cddfd11f5859c170632158
asp143 <mungcs09@gmail.com>
2020-10-06 09:30:27 +0800

    Added sample test case

---
+++ __test__/function/User.function.spec.ts
@@ -0,0 +1,15 @@
+import { doesNotMatch } from 'assert';
+jest.mock('../../src/model/User.model');
+import UserFunc from '../../src/function/User.function';
+
+
+/**
+ * This should hold all the Test cases for the user function file.
+ */
+describe("Test cases for User function", () => {
+    test('Find payload is undefined because there are no user that matches the query', async (done) => {
+        const result = await UserFunc.Find({ username: 'john' });
+        expect(result.payload).toBe(undefined);
+        done();
+    });
+});
