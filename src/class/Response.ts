5cb6dd587445e61a1d39230d05ef28c26a2292fc
asp143 <mungcs09@gmail.com>
2020-10-19 22:41:16 +0800

    Updated Response file

--- src/class/Response.ts
+++ src/class/Response.ts
@@ -7,13 +7,13 @@ export default class Response {
         // ... 
     }
 
-    public setFailed(status: number, payload: any): void {
+    public setFailed(status: number, payload: Record<string, unknown>): void {
         this.status = status;
         this.payload = payload;
         this.success = false;
     }
 
-    public setPayload(status: number, payload: any): void {
+    public setPayload(status: number, payload: Record<string, unknown>): void {
         this.status = status;
         this.payload = payload;
     }
